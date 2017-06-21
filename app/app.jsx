634382e24b6e5dcb3fd9d30dda4ce2d0a07f90e8
Darren Chan <darrennchan8@Gmail.com>
2017-06-21 14:23:26 -0400

    Implement main 'slideshow' view

--- app/app.jsx
+++ app/app.jsx
@@ -1,9 +1,74 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// The main wrapper function for encapsulating different 'fragments'
+import SlideShow from './app/ui';
+// One particular fragment. The format of this (and subsequent imports) will change soon.
 import authenticate from './app/authentication';
 
-authenticate({
-    parentElement: document.querySelector('#react-root')
-}).catch(function(err) {
-    console.error(err);
-}).then(function(details) {
-    console.log(details);
-});
+
+class TestApplication1 extends React.Component {
+	constructor(props) {
+		super(props);
+		console.warn('init');
+		setTimeout(function() {
+			this.props.onFinished({
+				success: true,
+				details: {
+					data: 'junk'
+				}
+			});
+		}.bind(this), 1000);
+	}
+	render() {
+		return (
+			<div>
+				<p>This is an example of a potential fragment...</p>
+			</div>
+		);
+	}
+}
+
+class TestApplication2 extends React.Component {
+	constructor(props) {
+		super(props);
+		console.warn('init');
+		setTimeout(function() {
+			this.props.onFinished({
+				success: false,
+				error: new Error('IO Exception')
+			});
+		}.bind(this), 1000);
+	}
+	render() {
+		return (
+			<div>
+				<p>This is the second fragment.</p>
+			</div>
+		);
+	}
+}
+
+let testApplication1 = <TestApplication1></TestApplication1>;
+let testApplication2 = <TestApplication2></TestApplication2>;
+
+const applicationPath = [{
+	element: testApplication1,
+	callback: function(details) {
+		console.log(details);
+	}
+}, {
+	element: testApplication2,
+	callback: function(details) {
+		console.log(details);
+	}
+}];
+
+const slideShow = (
+	<SlideShow
+		views={applicationPath}
+	>
+	</SlideShow>
+);
+
+ReactDOM.render(slideShow, document.querySelector('#react-root'));
---
+++ app/ui.jsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import $ from 'jquery';
+
+/*
+    Wrapper function/Factory for generating a new wrapper
+*/
+module.exports = class SlideShow extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            views: props.views, // An array that contains all of our views. This is accessed by reference.
+            viewIndex: 0, // The current View index
+            transition: null, // A function (or falsey value) that serves as a callback for onTransitionEnd
+            transitionElem: null // A View (or falsey value) that gets animated in.
+        };
+        this.cachedViews = {};
+    }
+    getView(index) {
+        return this.cachedViews[index] || (this.cachedViews[index] = // This is so that we don't clone an element twice.
+            (function(awaitingCallback) {
+                return React.cloneElement(this.state.views[index].element, {
+                    onFinished: function(details) {
+                        if (!awaitingCallback) {
+                            return;
+                        }
+                        awaitingCallback = false;
+                        try {
+                            this.state.views[index].callback(details);
+                        } catch (err) {
+                            console.error('Error in callback to slide ' + index, err);
+                        }
+                        if (this.state.viewIndex < this.state.views.length - 1) {
+                            this.nextPage();
+                        }
+                    }.bind(this)
+                });
+            }.bind(this))(true));
+    }
+    render() {
+        return (
+            <div
+                style={{
+                    alignItems: 'center',
+                    display: 'flex',
+                    height: '100%',
+                    justifyContent: 'center',
+                    position: 'fixed',
+                    width: '100%'
+                }}
+            >
+                <style>
+                    {`
+                        div.fixed {
+                            transform: translateX(0);
+                        }
+                        div.right {
+                            transform: translateX(100vw);
+                        }
+                    `}
+                </style>
+                <div
+                    className="fixed"
+                    style={{
+                        alignItems: 'center',
+                        display: 'flex',
+                        height: '100%',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        position: 'absolute',
+                        transform: this.state.transition ? 'translateX(-100vw)' : null,
+                        transition: this.state.transition ? 'transform 500ms ease-in-out' : null,
+                        width: '100%',
+                        zIndex: '1'
+                    }}
+                    onTransitionEnd={this.state.transition}
+                >
+                    <div
+                        style={{
+                            position: 'relative',
+                            width: '100%',
+                            height: '100%',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center'
+                        }}
+                    >
+                        <div
+                            style={{
+                                position: 'absolute'
+                            }}
+                        >
+                            {this.getView(this.state.viewIndex)}
+                        </div>
+                    </div>
+                    <div
+                        style={{
+                            position: 'absolute',
+                            transform: 'translateX(100vw)'
+                        }}
+                    >
+                        {this.state.transitionElem}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    nextPage() {
+        this.setState({
+            transitionElem: this.getView(this.state.viewIndex + 1),
+            transition: function() {
+                // Async callback after transition ends
+                this.setState({
+                    transition: null,
+                    transitionElem: null,
+                    viewIndex: this.state.viewIndex + 1
+                });
+            }.bind(this)
+        });
+    }
+}
